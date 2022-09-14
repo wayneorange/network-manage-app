8829640a0e4dae435157ed9efa63c070b6dba8c2
chenwei <chenweiwool@163.com>
2022-09-14 11:22:13 +0800

    feat：代码更新

--- views/network/network.vue
+++ views/network/network.vue
@@ -1,7 +1,10 @@
 <template>
 	<view class="network-container">
 		<canvas id="canvas" canvas-id="canvas" width="100%" height="100%"></canvas>
-		<img src="static/network/luyou.png" class="img">
+		<text style="" class="luyou">路由器</text>
+		<view class="text-contetnt">
+			<text :style="{height: `${110 + ((Math.ceil((i + 1) / 4) - 1)) * 5}px`}" v-for="(item, i) in imgs" :key="i + item.text">{{ item.text }}</text>
+		</view>
 	</view>
 </template>
 
@@ -13,6 +16,28 @@
 				nodes: [
 					{
 						title: ''
+					}
+				],
+				imgs: [
+					{
+						text: 'My Phone',
+						img: '../../static/network/apple.png'
+					},
+					{
+						text: 'Dell',
+						img: '../../static/network/dell.png'
+					},
+					{
+						text: 'Dell',
+						img: '../../static/network/dell.png'
+					},
+					{
+						text: 'Dell',
+						img: '../../static/network/dell.png'
+					},
+					{
+						text: 'Dell',
+						img: '../../static/network/dell.png'
 					}
 				]
 			};
@@ -26,18 +51,63 @@
 				const screenInfo = uni.getSystemInfoSync();
 				// uniapp选择元素节点
 				const query = uni.createSelectorQuery().in(this);
-				const width = screenInfo.windowWidth;
+				const screenWidth = screenInfo.windowWidth;
+				const color = '#4875b3';
 				const ctx = uni.createCanvasContext('canvas');
-				console.log(screenInfo);
+				console.log('screenInfo', screenInfo);
 				// 
-				ctx.drawImage('../../static/network/network.png', (width / 2) - 40, 40, 80, 80);
-				ctx.drawImage('../../static/network/luyou.png', (width / 2) - 50, 200, 100, 100);
-				ctx.moveTo((width / 2), 200);
-				ctx.lineTo(300,120);
-				ctx.lineTo(300,600);
-				ctx.lineWidth=10;
-				ctx.strokeStyle="blue";
+				const centerScreen = Number(screenWidth / 2);
+				ctx.drawImage('../../static/network/network.png', centerScreen - 40, 40, 80, 80);
+				ctx.drawImage('../../static/network/luyou.png', centerScreen - 50, 180, 100, 100);
+				ctx.moveTo(centerScreen, 180);
+				ctx.lineTo(centerScreen, 150);
+				ctx.lineWidth = 2.5;
+				ctx.strokeStyle= color;
 				ctx.stroke();
+				
+				ctx.beginPath()
+				ctx.arc(centerScreen, 150, 3, 0, 2 * Math.PI)
+				ctx.setFillStyle(color);
+				ctx.stroke();
+				ctx.fill();
+				ctx.closePath()
+				
+				const imgs = this.imgs || [];
+				const num = ((screenWidth / 4) - 80) / 2;
+				imgs.forEach((o, i) => {
+					ctx.drawImage(o.img, ((screenWidth / 4) * (i % 4)) + num, (Math.ceil((i + 1) / 4) - 1) * 120 + 350 , 80, 80);
+				})
+				
+				const startLine = (screenWidth / 4) / 2;
+				const endLine = screenWidth - startLine;
+				ctx.beginPath();
+				ctx.moveTo(startLine, 350);
+				ctx.lineTo(startLine, 345);
+								
+				// 画弧线
+				ctx.arcTo(startLine, 330, startLine + 5, 330, 5); // 创建弧
+				
+				ctx.moveTo(startLine + 5, 330);
+				ctx.lineTo(centerScreen, 330);
+				ctx.lineTo(centerScreen, 310);
+				ctx.lineTo(centerScreen, 330);
+				ctx.lineTo(endLine - 5, 330);
+				
+				// 画弧线
+				ctx.arcTo(endLine, 330, endLine, 335, 5); // 创建弧
+
+				ctx.lineTo(endLine, 350);
+				ctx.lineWidth = 2.5;
+				ctx.strokeStyle= '#cecece';
+				ctx.stroke();
+				ctx.closePath();
+				
+				ctx.beginPath();
+				ctx.arc(centerScreen, 310, 4, 0, 2 * Math.PI)
+				ctx.setFillStyle('#cecece');
+				ctx.closePath();
+				ctx.fill();
+				
 				ctx.draw()
 			
 			}
@@ -49,12 +119,41 @@
 .network-container {
 	width: 100%;
 	height: 100%;
-	border: 1px solid red;
 	background: #FFFFFF;
+	position: relative;
+	
+	.luyou {
+		left: 50%;
+		transform: translateX(-50%);
+		top: 280px;
+		position: absolute;
+	}
+	
+	.text-contetnt {
+		position: absolute;
+		top: 350px;
+		width: 100%;
+		display: grid;
+		grid-template-columns: 25% 25% 25% 25%;
+		overflow: hidden;
+		
+		text {
+			height: 110px;
+			display: flex;
+			align-items: flex-end;
+			justify-content: center;
+		}
+	}
+	
+	text {
+		font-size: 1.25em;
+		font-weight: 500;
+	}
 	
 	uni-canvas {
 		height: 100%;
 		width: 100%;
 	}
+	
 }
 </style>
--- views/toolbox/toolbox.vue
+++ views/toolbox/toolbox.vue
@@ -1,6 +1,14 @@
 <template>
-	<view>
-		<span>这是工具箱界面</span>
+	<view class="tooltip-contaier">
+		<view class="tooltip-list" v-for="(item, i) in list" :key="item.title + i">
+			<view class="left">
+				<img :src="item.img">
+				<text class="name">{{ item.title }}</text>
+			</view>
+			<view class="right">
+				<u-icon name="arrow-right"></u-icon>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +16,85 @@
 	export default {
 		data() {
 			return {
-				
+				list: [
+					{
+						title: '九轴测试图',
+						img: '../../static/network/dell.png',
+						url: ''
+					},
+					{
+						title: '温湿度图',
+						img: '../../static/network/dell.png',
+						url: ''
+					},
+					{
+						title: '路由器测试',
+						img: '../../static/network/dell.png',
+						url: ''
+					},
+					{
+						title: '图表历史记录',
+						img: '../../static/network/dell.png',
+						url: ''
+					},
+					{
+						title: '实时环境检测历史记录',
+						img: '../../static/network/dell.png',
+						url: ''
+					},
+					{
+						title: '实时姿态检测历史记录',
+						img: '../../static/network/dell.png',
+						url: ''
+					}
+				]
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.tooltip-contaier {
+	height: 100%;
+	width: 100%;
+	background-color: #fff;
+	overflow-y: scroll;
+	
+	.tooltip-list {
+		height: 80px;
+		width: 100%;
+		border-bottom: 1px solid #dadce0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		
+		.left {
+			height: 100%;
+			display: flex;
+			align-items: center;
+			margin-left: 20px;
+			
+			img {
+				height: 60px;
+				width: 60px;
+				border-radius: 10px;
+				margin-right: 20px;
+			}
+			
+			.name {
+				font-size: 1.25em;
+				font-weight: 600;
+			}
+		}
+		
+		.right {
+			height: 100%;
+			display: flex;
+			align-items: center;
+			margin-right: 20px;
+		}
+		
+		
+	}
+}
 </style>
